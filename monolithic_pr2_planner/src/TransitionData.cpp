3484885a4b2f3763e0427ad9bd4a845c5a3c1aca
Siddharth Swaminathan <sid24ss@gmail.com>
2014-08-25 14:36:07 -0400

    fixed issue where it crashed due to incorrect vector combining

--- monolithic_pr2_planner/src/TransitionData.cpp
+++ monolithic_pr2_planner/src/TransitionData.cpp
@@ -1,4 +1,5 @@
 #include <monolithic_pr2_planner/TransitionData.h>
+#include <monolithic_pr2_planner/LoggerNames.h>
 
 using namespace monolithic_pr2_planner;
 
@@ -6,8 +7,13 @@ TransitionData TransitionData::combineTData(const TransitionData t_data1,
                const TransitionData t_data2)
 {
     TransitionData t_data;
+
+    // the id of the end robot state, which comes after t_data2.
     int successor_id = t_data2.successor_id();
     t_data.successor_id(successor_id);
+    
+    // set the motion type. We want this to default to base when there is a base
+    // movement
     bool isBaseType = (t_data1.motion_type() == MPrim_Types::BASE) ||
                         (t_data2.motion_type() == MPrim_Types::BASE);
     if (isBaseType) {
@@ -16,16 +22,23 @@ TransitionData TransitionData::combineTData(const TransitionData t_data1,
         t_data.motion_type(t_data2.motion_type());
     }
 
+    // set the intermediate robot steps
+    ROS_DEBUG_NAMED(POSTPROCESSOR_LOG, "setting robot steps");
     std::vector<RobotState> steps(t_data1.interm_robot_steps());
-    steps.insert(steps.end(), t_data2.interm_robot_steps().begin(), t_data2.
-        interm_robot_steps().end());
+    for (auto step : t_data2.interm_robot_steps()) {
+        steps.push_back(step);
+    }
     t_data.interm_robot_steps(steps);
 
+    // set the base steps
+    ROS_DEBUG_NAMED(POSTPROCESSOR_LOG, "setting base steps");
     std::vector<ContBaseState> base_steps(t_data1.cont_base_interm_steps());
-    base_steps.insert(base_steps.end(), t_data2.cont_base_interm_steps().begin(), t_data2.
-        cont_base_interm_steps().end());
+    for (auto step : t_data2.cont_base_interm_steps()) {
+        base_steps.push_back(step);
+    }
     t_data.cont_base_interm_steps(base_steps);
 
+    // set the cost
     int total_cost = t_data1.cost();
     total_cost += t_data2.cost();
     t_data.cost(total_cost);
